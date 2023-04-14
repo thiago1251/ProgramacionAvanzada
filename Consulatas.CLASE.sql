00d8f9e27106d2913133fbdd6ef414a2274a8925
thiago1251 <115585014+thiago1251@users.noreply.github.com>
2023-04-14 18:33:47 -0500

    Add files via upload

---
+++ Consulatas.CLASE.sql
@@ -0,0 +1,81 @@
+SELECT Nombre, Telefono 
+FROM profesores;
+
+SELECT Nombre, Telefono 
+FROM programas
+WHERE Depto = 1 ;
+
+SELECT idprofesor, idproyecto
+FROM Participacion_proyectos
+WHERE idprofesor = 3333;
+
+SELECT idprofesor, idproyecto
+FROM Participacion_proyectos
+WHERE idproyecto = 2;
+
+SELECT idprofesor, idproyecto, Horas
+FROM Participacion_proyectos
+WHERE Horas > 5;
+
+SELECT idprofesor, idproyecto, Horas
+FROM Participacion_proyectos
+WHERE Horas >= 4 AND Horas <= 6;
+
+SELECT idprofesor, idproyecto, Horas
+FROM Participacion_proyectos
+WHERE Horas BETWEEN 4 AND 6;
+
+UPDATE departamentos 
+SET jefe = 3333
+WHERE idDepartamento = 1;
+
+SELECT * 
+FROM programas
+WHERE Depto = 1 or Depto = 3 ;
+
+SELECT * 
+FROM programas
+WHERE Depto in(1,3) ;
+
+SELECT idProyecto, Presupuesto
+FROM proyectos;
+
+SELECT idProyecto, Presupuesto
+FROM proyectos
+ORDER BY presupuesto;
+
+SELECT idProyecto, Presupuesto
+FROM proyectos
+ORDER BY presupuesto;
+
+-- DOS TABLAS
+
+
+SELECT profesores.Nombre AS PROFESOR, programas.Nombre AS PROGRAMA
+FROM profesores INNER JOIN programas
+ON programas.idPrograma = profesores.Programa;
+
+SELECT profesores.nombre AS 'PROFESOR JEFE', departamentos.nombre AS DEPARTAMENTO
+FROM profesores INNER JOIN departamentos
+ON profesores.idProfesor = departamentos.Jefe;
+
+
+SELECT proyectos.Nombre , profesores.Nombre AS 'PROFESOR LIDER', proyectos.Presupuesto
+FROM proyectos INNER JOIN profesores
+ON profesores.idProfesor = proyectos.Lider;
+
+-- TRES TABLAS 
+
+SELECT profesores.nombre AS Profe, proyectos.nombre as Proy, participacion_proyectos.horas as Horas
+FROM(participacion_proyectos INNER JOIN profesores
+ON participacion_proyectos.idProfesor = Profesores.idProfesor) 
+INNER JOIN proyectos
+ON participacion_proyectos.idProyecto = proyectos.idProyecto;
+
+SELECT Fuentes.Nombre, proyectos.Nombre, monto
+FROM  (financiacion_proyectos INNER JOIN proyectos
+ON financiacion_proyectos.idProyecto = proyectos.idProyecto)
+INNER JOIN fuentes
+ON financiacion_proyectos.idFuente= fuentes.idFuente;
+
+-- nombre fuente, nombre proyecto, monto
